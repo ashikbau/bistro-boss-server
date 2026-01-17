6a518be1de22fcb6b3156788a33719021c63e9c8
Asraful Alam <alamgu4@gmail.com>
2026-01-18 00:44:40 +0100

    ready for vercel deploy

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 const allowedOrigins = [
-  "http://localhost:5173", // frontend during local dev
+  "https://bistro-boss-client-c0d0d.web.app",
 
 ];
 
